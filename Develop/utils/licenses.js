9d85606a054f8bd20a615549259a34be8d9838c3
Daniel Packer <danielapacker@gmail.com>
2022-10-21 19:12:39 -0700

    finish adding licenses

--- Develop/utils/licenses.js
+++ Develop/utils/licenses.js
@@ -25,7 +25,7 @@ const licenses = {
     },
     "Boost Software License 1.0": {
         licenseLink: "https://www.boost.org/LICENSE_1_0.txt",
-        licenseImg: "hhttps://img.shields.io/badge/License-Boost_1.0-lightblue.svg"
+        licenseImg: "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg"
     },
     "Creative Commons Zero v1.0 Universal": {
         licenseLink: "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
@@ -34,5 +34,25 @@ const licenses = {
     "Eclipse Public License 2.0": {
         licenseLink: "https://www.eclipse.org/legal/epl-2.0/",
         licenseImg: "https://img.shields.io/badge/License-EPL_2.0-red.svg"
+    },
+    "GNU Affero General Public License v3.0": {
+        licenseLink: "https://www.gnu.org/licenses/gpl-3.0",
+        licenseImg: "https://img.shields.io/badge/License-GPLv3-blue.svg"
+    },
+    "GNU General Public License v2.0": {
+        licenseLink: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+        licenseImg: "https://img.shields.io/badge/License-GPL_v2-blue.svg"
+    },
+    "GNU Lesser General Public License v2.1": {
+        licenseLink: "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html",
+        licenseImg: "https://img.shields.io/badge/License-LGPL_v2.1-blue.svg"
+    },
+    "Mozilla Public License 2.0l": {
+        licenseLink: "https://opensource.org/licenses/MPL-2.0",
+        licenseImg: "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg"
+    },
+    "The Unlicense": {
+        licenseLink: "http://unlicense.org/",
+        licenseImg: "https://img.shields.io/badge/license-Unlicense-blue.svg"
     },
 }
